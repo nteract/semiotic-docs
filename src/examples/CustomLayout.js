6f7aeb2149b490a8db0a730d877c4c18f831f5c3
micahstubbs <micah.stubbs@gmail.com>
2019-08-26 15:20:06 -0700

    commit output from copy full code on docs page

--- src/examples/CustomLayout.js
+++ src/examples/CustomLayout.js
@@ -1,137 +1,106 @@
-import React from "react"
-import DocumentFrame from "../DocumentFrame"
-import { NetworkFrame } from "semiotic"
-import theme from "../theme"
-import MarkdownText from "../MarkdownText"
-import flextree from "d3-flextree-v4"
-
-const tree = {
-  name: "root",
-  height: 25,
-  width: 100,
-  children: [
-    {
-      name: "a",
-      height: 40,
-      width: 40,
-      children: [
-        {
-          name: "b",
-          height: 80,
-          width: 40,
-          children: [
-            {
-              name: "c",
-              height: 40,
-              width: 100,
-              children: [{ name: "d", height: 200, width: 40 }]
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: "e",
-      height: 40,
-      width: 40,
-      children: [
-        {
-          name: "f",
-          height: 40,
-          width: 40,
-          children: [
-            {
-              name: "g",
-              height: 40,
-              width: 200,
-              children: [
-                { name: "h", height: 60, width: 30 },
-                { name: "h1", height: 80, width: 30 },
-                { name: "h2", height: 40, width: 30 }
-              ]
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: "i",
-      height: 40,
-      width: 40,
-      children: [
-        {
-          name: "j",
-          height: 40,
-          width: 100,
-          children: [{ name: "k", height: 200, width: 40 }]
-        }
-      ]
-    }
-  ]
-}
-
-const flexLayoutSize = [700, 700]
-//Because flextree returns nodes spaced in a way unlike the standard d3-hierarchy layouts, we need to pass a custom function to the networkType zoom prop. That function is passed (nodes, size) and you can adjust the position of the nodes accordingly
-const flextreeZoom = (nodes, size) => {
-  const minX = Math.min(...nodes.map(node => node.x - node.width / 2))
-  const maxX = Math.max(...nodes.map(node => node.x + node.width / 2))
-  const minY = Math.min(...nodes.map(node => node.y))
-  const maxY = Math.max(...nodes.map(node => node.y + node.height))
-
-  const xScalingFactor = size[0] / (maxX - minX)
-  const yScalingFactor = size[1] / (maxY - minY)
-
-  nodes.forEach(node => {
-    //This is enough to display on screen
-    node.x = node.x + Math.abs(minX)
-    //But if you wanted to zoom-to-fit you can do this:
-    node.x = node.x * xScalingFactor
-    node.data.width = node.data.width * xScalingFactor
-    node.y = node.y * yScalingFactor
-    node.data.height = node.data.height * yScalingFactor
-  })
-}
-
+import NetworkFrame from "semiotic/lib/NetworkFrame"
+const theme = [
+  "#ac58e5",
+  "#E0488B",
+  "#9fd0cb",
+  "#e0d33a",
+  "#7566ff",
+  "#533f82",
+  "#7a255d",
+  "#365350",
+  "#a19a11",
+  "#3f4482"
+]
 const frameProps = {
-  size: flexLayoutSize,
-  edges: tree,
+  edges: {
+    name: "root",
+    height: 38.961038961038966,
+    width: 166.66666666666669,
+    children: [
+      {
+        name: "a",
+        height: 62.33766233766234,
+        width: 66.66666666666667,
+        children: [
+          {
+            name: "b",
+            height: 124.67532467532467,
+            width: 66.66666666666667,
+            children: [
+              {
+                name: "c",
+                height: 62.33766233766234,
+                width: 166.66666666666669,
+                children: [
+                  {
+                    name: "d",
+                    height: 311.6883116883117,
+                    width: 66.66666666666667
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "e",
+        height: 62.33766233766234,
+        width: 66.66666666666667,
+        children: [
+          {
+            name: "f",
+            height: 62.33766233766234,
+            width: 66.66666666666667,
+            children: [
+              {
+                name: "g",
+                height: 62.33766233766234,
+                width: 333.33333333333337,
+                children: [
+                  { name: "h", height: 93.50649350649351, width: 50 },
+                  { name: "h1", height: 124.67532467532467, width: 50 },
+                  { name: "h2", height: 62.33766233766234, width: 50 }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "i",
+        height: 62.33766233766234,
+        width: 66.66666666666667,
+        children: [
+          {
+            name: "j",
+            height: 62.33766233766234,
+            width: 166.66666666666669,
+            children: [
+              { name: "k", height: 311.6883116883117, width: 66.66666666666667 }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  size: [700, 700],
+  margin: 50,
+  networkType: {
+    zoom: flextreeZoom,
+    type: "tree",
+    layout: flextree,
+    nodeSize: d => [d.data.width, d.data.height],
+    spacing: 10
+  },
+  nodeIDAccessor: "name",
   edgeStyle: d => ({
     fill: theme[d.source.depth],
     stroke: theme[d.source.depth],
     strokeWidth: 5,
     opacity: 0.5
   }),
-  nodeIDAccessor: "name",
-  hoverAnnotation: true,
   customNodeIcon: ({ d }) => {
-    return (
-      <rect
-        x={d.x - d.data.width / 2}
-        y={d.y - 10}
-        height={d.data.height - 10}
-        width={d.data.width}
-        fill={theme[2]}
-      />
-    )
-  },
-  networkType: {
-    zoom: flextreeZoom,
-    type: "tree",
-    layout: flextree,
-    nodeSize: d => [d.data.width, d.data.height],
-    spacing: () => 10
-  },
-  margin: 50
-}
-
-const overrideProps = {
-  edgeStyle: `d => ({
-    fill: theme[d.source.depth],
-    stroke: theme[d.source.depth],
-    strokeWidth: 5,
-    opacity: 0.5
-  })`,
-  customNodeIcon: `({ d }) => {
     return (
       <rect
         x={d.x - d.data.width / 2}
@@ -142,31 +111,10 @@ const overrideProps = {
         stroke="gold"
       />
     )
-  }`,
-  networkType: `{
-    zoom: flextreeZoom,
-    type: "tree",
-    layout: flextree,
-    nodeSize: d => [d.data.width, d.data.height],
-    spacing: 10
-  }`
+  },
+  hoverAnnotation: true
 }
 
-export default function CustomLayout() {
-  return (
-    <div>
-      <MarkdownText
-        text={`
-
-This example shows how you can use \`NetworkFrame\` with a custom \`layout\` function to position \`nodes\` and \`edges\`.
-`}
-      />
-      <DocumentFrame
-        frameProps={frameProps}
-        overrideProps={overrideProps}
-        type={NetworkFrame}
-        useExpanded
-      />
-    </div>
-  )
+export default () => {
+  return <NetworkFrame {...frameProps} />
 }
